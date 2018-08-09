d64b8f7d79d6418ee297450364035bd2ba7f6737
YOYO1HU9 <121189275@qq.com>
2018-08-09 17:36:58 +0800

    :'check'

--- readfile.py
+++ readfile.py
@@ -1,10 +1,24 @@
 from docx import Document
+import json
 import pprint
 
 document = Document('source.docx')
 
 document = [part.text for part in document.paragraphs]
 
-pprint.pprint(document)
+doc = {}
+x = 1
+key = ''
+value = ''
+for line in document:
+    if line.startswith("{}、".format(x)):
+        if key:
+            doc[key] = value
+        value = ''
+        key = line
+        x += 1
+    else:
+        value += line
 
-
+with open('temp.doc', 'w') as file:
+    json.dump(doc, file)
