bd7551737634539b5c2710d8511233697af2fa4d
YOYO1HU9 <121189275@qq.com>
2018-08-11 23:13:13 +0800

    the second dictionary

--- readfile.py
+++ readfile.py
@@ -6,19 +6,34 @@ document = Document('source.docx')
 
 document = [part.text for part in document.paragraphs]
 
+def time_arrangement(event):
+    z = event.find('）')
+    y = event.find('，')
+    return event[z+1:y]
+
 doc = {}
 x = 1
 key = ''
-value = ''
+value = []
 for line in document:
     if line.startswith("{}、".format(x)):
-        if key:
+        if key :
             doc[key] = value
-        value = ''
+        value = []
         key = line
         x += 1
     else:
-        value += line
+        value.append(line)
+        key_2 = ''
+        doc_2 = {}
+        if key:
+            if time_arrangement(line):
+                key_2 = time_arrangement(line)
+                doc_2[key_2] = line
 
-with open('temp.txt', 'w', encoding='utf8') as file:
-    json.dump(doc, file, ensure_ascii=False)
+                print(doc_2)
+
+
+
+#with open('temp.txt', 'w', encoding='utf8') as file:
+   # json.dump(doc, file, ensure_ascii=False)
